28467c3b3163d673cf63af66354171e0a7b28994
Lamarcgasolridge <80485596+Lamarcgasolridge@users.noreply.github.com>
2021-08-31 10:31:10 -0700

    Complete Pypoll
    Pypoll portion complete, pypoll analysis file added

--- Instructions/PyPoll/main_pypoll.py
+++ Instructions/PyPoll/main_pypoll.py
@@ -20,7 +20,8 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
         if row[2] not in candidates:
             candidates.append(row[2])
-        print(f'Our candidates are: {candidates}')
+    
+print(f'Our candidates are: {candidates}')
 
 #Count the votes
 with open(csvpath, newline='') as csvfile:
@@ -38,4 +39,37 @@ with open(csvpath, newline='') as csvfile:
 
 tot_votes = khan + correy + li + otooley
 
+#Vote share calculation
+khan_share = khan / tot_votes
+correy_share = correy / tot_votes
+li_share = li / tot_votes
+otooley_share = otooley / tot_votes
 
+#Find winner
+winner = max(khan, correy, li, otooley)
+
+if winner == khan:
+    winner_id = "Khan"
+elif winner == correy:
+    winner_id = "Correy"
+elif winner == li:
+    winner_id = "Li"
+elif winner == otooley:
+    winner_id = "O'Tooley"
+
+#Output Document
+output_path = 'Pypoll Analysis.txt'
+
+with open(output_path, 'w', newline = '') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(["------------------------------------------------------"])
+    csvwriter.writerow([f'Total Votes: {tot_votes}'])
+    csvwriter.writerow(["------------------------------------------------------"])
+    csvwriter.writerow([f'Khan: {khan_share:.2%}({khan})'])
+    csvwriter.writerow([f'Correy: {correy_share:.2%}({correy})'])
+    csvwriter.writerow([f'Li: {li_share:.2%}({li})'])
+    csvwriter.writerow([f"O'Tooley: {otooley_share:.2%}({otooley})"])
+    csvwriter.writerow(["------------------------------------------------------"])
+    csvwriter.writerow([f"Winner: {winner_id}"])
+    csvwriter.writerow(["------------------------------------------------------"])
